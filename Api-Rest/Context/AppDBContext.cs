cf4345356a7765c0613a9869799c353598ce1e1c
LeoZamora <leozamora2003@outlook.com>
2024-05-20 11:35:24 -0600

    firts commit

---
+++ Api-Rest/Context/AppDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Api_Rest.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Api_Rest.Context
+{
+    public class AppDBContext: DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options): base(options) 
+        { 
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ Api-Rest/Models/Cliente.cs
@@ -0,0 +1,10 @@
+﻿namespace Api_Rest.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Lastname { get; set; }
+        public required int Age { get; set; }
+    }
+}
